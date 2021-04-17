67bf14ae0d7fa5e49caeaefea5b63ab8d41a2ee1
tme5 <tusharmangale1991@gmail.com>
2021-04-17 22:53:44 +0530

    Enabled left click in static maze

--- agneepath.py
+++ agneepath.py
@@ -130,6 +130,96 @@ class Agneepath:
                     if event.type == pygame.QUIT:
                         run = False
                         return
+                        
+                    if pygame.mouse.get_pressed()[0]: # LEFT
+                        pos = pygame.mouse.get_pos()
+                        row, col = get_clicked_pos(pos, TOTAL_ROWS, WIDTH)
+                        spot = grid[row][col]
+                        if not start and spot != end:
+                            start = spot
+                            start.make_start()
+                            
+                        elif not end and spot != start:
+                            end = spot
+                            end.make_end()
+
+                        elif spot != end and spot != start:
+                            spot.make_barrier()
+
+                    elif pygame.mouse.get_pressed()[2]: # RIGHT
+                        pos = pygame.mouse.get_pos()
+                        row, col = get_clicked_pos(pos, TOTAL_ROWS, WIDTH)
+                        spot = grid[row][col]
+                        spot.reset()
+                        if spot == start:
+                            start = None
+                        elif spot == end:
+                            end = None
+
+                    if event.type == pygame.KEYDOWN:
+                        if event.key == pygame.K_ESCAPE:
+                            run = False
+                            return
+                        if event.key == pygame.K_SPACE and start and end:
+                            find_path = True
+                            count = 0
+                if delay:
+                    count -=1
+            else:
+                if find_path:
+                    for row in grid:
+                        for spot in row:
+                            spot.update_neighbors(grid)
+                    astar_path = algorithm(lambda: draw(self.win, grid, TOTAL_ROWS, WIDTH), grid, end, start)
+                    find_path = False
+                if astar_path:
+                    if len([spot for spot in astar_path if spot.is_barrier]) > 0:
+                        for spot in astar_path:
+                            if not spot.is_barrier:
+                                spot.reset()
+                        find_path = True
+                    if not find_path:
+                        if start in astar_path:
+                            start.reset()
+                            start = astar_path.pop(start)
+                            start.make_start()
+                            delay = True
+                            count = COUNT
+                        if start is end:
+                            end.make_end()
+                            delay = False
+                            count = COUNT
+
+    def dynamic_maze(self):
+        grid = make_grid(TOTAL_ROWS, WIDTH)
+        with open(MAZE_CONF, 'r') as f:
+            f_rd = f.readlines()
+        i = 0
+        for ln in f_rd:
+            j = 0
+            for conf in ln.split(','):
+                if int(conf):
+                    grid[j][i].make_barrier()
+                j += 1
+            i += 1
+        
+        grid[1][0].make_start()
+        grid[19][18].make_end()
+
+        start = grid[1][0]
+        end = grid[19][18]
+        astar_path = False
+        find_path = False
+        run = True
+        count = COUNT
+        delay = False
+        while run:
+            draw(self.win, grid, TOTAL_ROWS, WIDTH)
+            if count > 0:
+                for event in pygame.event.get():
+                    if event.type == pygame.QUIT:
+                        run = False
+                        return
                     if event.type == pygame.KEYDOWN:
                         if event.key == pygame.K_ESCAPE:
                             run = False
