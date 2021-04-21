42713b230f39ff580c17d93027a7021b7ccb84fe
tme5 <tusharmangale1991@gmail.com>
2021-04-21 21:07:42 +0530

    Added multiple end configuration

--- agneepath.py
+++ agneepath.py
@@ -30,80 +30,115 @@ class Agneepath:
         surface.blit(textobj, textrect)
         
     def main(self):
+        i = 0
         click = False
         mainClock = pygame.time.Clock()
         
         bg = pygame.image.load(BG_IMAGE)
         logo = pygame.image.load(LOGO)
-        active = True
-        user_input = ""
+        textbox1_active = False
+        textbox2_active = False
+        user_input1 = '1'
+        user_input2 = '1'
         self.start_count = 1
+        self.end_count = 1
 
         while True:
+            textbox1_border = BUTTON_BORDER
+            textbox2_border = BUTTON_BORDER
+            button1_border = BUTTON_BORDER
+            button2_border = BUTTON_BORDER
+            button3_border = BUTTON_BORDER
+            button4_border = BUTTON_BORDER
+            button5_border = BUTTON_BORDER
             self.win.fill((0,0,0))
             self.win.blit(bg, (0,0))
             self.draw_text('AGNEEPATH', self.title, BUTTON, self.win, 80, 10)
             self.draw_text('MAIN MENU', self.font1, YELLOW, self.win, 200, 150)
             self.draw_text('Enter start count', self.font4, YELLOW, self.win, 300, 90)
+            self.draw_text('Enter end count', self.font4, YELLOW, self.win, 300, 130)
 
             self.win.blit(logo, (10, 10))
             mx, my = pygame.mouse.get_pos()
 
-            textbox = pygame.Rect(500, 90, 50, 30)
+            textbox1 = pygame.Rect(500, 90, 50, 30)
+            textbox2 = pygame.Rect(500, 130, 50, 30)
             button_1 = pygame.Rect(200, 200, 220, 50)
             button_2 = pygame.Rect(200, 260, 220, 50)
             button_3 = pygame.Rect(200, 320, 220, 50)
             button_4 = pygame.Rect(200, 380, 220, 50)
             button_5 = pygame.Rect(235, 440, 150, 50)
 
-            if textbox.collidepoint((mx, my)):
+            if textbox1.collidepoint((mx, my)):
+                textbox1_border = WHITE
                 if click:
-                    active = not active
+                    textbox1_active = not textbox1_active
                     click = False
-                
+
+            if textbox2.collidepoint((mx, my)):
+                textbox2_border = WHITE
+                if click:
+                    textbox2_active = not textbox2_active
+                    click = False
+
             if button_1.collidepoint((mx, my)):
+                button1_border = WHITE
                 if click:
                     self.play_game()
                     click = False
             
             if button_2.collidepoint((mx, my)):
+                button2_border = WHITE
                 if click:
                     self.static_maze()
                     click = False
             
             if button_3.collidepoint((mx, my)):
+                button3_border = WHITE
                 if click:
                     self.dynamic_maze()
                     click = False
             
             if button_4.collidepoint((mx, my)):
+                button4_border = WHITE
                 if click:
                     self.custom_maze()
                     click = False
             
             if button_5.collidepoint((mx, my)):
+                button5_border = WHITE
                 if click:
                     pygame.quit()
                     sys.exit()
 
-            if active:
-                pygame.draw.rect(self.win, BUTTON, textbox)
+            if textbox1_active:
+                pygame.draw.rect(self.win, textbox1_border, textbox1, 5)
+                pygame.draw.rect(self.win, BUTTON, textbox1)
             else:
-                pygame.draw.rect(self.win, BUTTON_BORDER, textbox)
+                pygame.draw.rect(self.win, textbox1_border, textbox1, 5)
+                pygame.draw.rect(self.win, BUTTON_BORDER, textbox1)
+            
+            if textbox2_active:
+                pygame.draw.rect(self.win, textbox2_border, textbox2, 5)
+                pygame.draw.rect(self.win, BUTTON, textbox2)
+            else:
+                pygame.draw.rect(self.win, textbox2_border, textbox2, 5)
+                pygame.draw.rect(self.win, BUTTON_BORDER, textbox2)
+            
             pygame.draw.rect(self.win, BUTTON, button_1, 0, 30)
-            pygame.draw.rect(self.win, BUTTON_BORDER, button_1, 5, 30)
+            pygame.draw.rect(self.win, button1_border, button_1, 5, 30)
 
             pygame.draw.rect(self.win, BUTTON, button_2, 0, 30)
-            pygame.draw.rect(self.win, BUTTON_BORDER, button_2, 5, 30)
+            pygame.draw.rect(self.win, button2_border, button_2, 5, 30)
 
             pygame.draw.rect(self.win, BUTTON, button_3, 0, 30)
-            pygame.draw.rect(self.win, BUTTON_BORDER, button_3, 5, 30)
+            pygame.draw.rect(self.win, button3_border, button_3, 5, 30)
 
             pygame.draw.rect(self.win, BUTTON, button_4, 0, 30)
-            pygame.draw.rect(self.win, BUTTON_BORDER, button_4, 5, 30)
+            pygame.draw.rect(self.win, button4_border, button_4, 5, 30)
 
             pygame.draw.rect(self.win, BUTTON, button_5, 0, 30)
-            pygame.draw.rect(self.win, BUTTON_BORDER, button_5, 5, 30)
+            pygame.draw.rect(self.win, button5_border, button_5, 5, 30)
 
             self.draw_text('PLAY GAME', self.font2, YELLOW, self.win, 245, 207)
             self.draw_text('STATIC MAZE', self.font2, YELLOW, self.win, 235, 267)
@@ -119,19 +154,28 @@ class Agneepath:
                     if event.key == pygame.K_ESCAPE:
                         pygame.quit()
                         sys.exit()
-                    if active:
+                    if textbox1_active:
                         if event.key == pygame.K_RETURN:
-                            self.start_count = int(user_input)
-                            active = False
+                            self.start_count = int(user_input1)
+                            textbox1_active = False
                         elif event.key == pygame.K_BACKSPACE:
-                            user_input = user_input[:-1]
+                            user_input1 = user_input1[:-1]
                         else:
-                            user_input += event.unicode
+                            user_input1 += event.unicode
+                    if textbox2_active:
+                        if event.key == pygame.K_RETURN:
+                            self.end_count = int(user_input2)
+                            textbox2_active = False
+                        elif event.key == pygame.K_BACKSPACE:
+                            user_input2 = user_input2[:-1]
+                        else:
+                            user_input2 += event.unicode
                 if event.type == pygame.MOUSEBUTTONDOWN:
                     if event.button == 1:
                         click = True
 
-            self.draw_text(user_input, self.font3, YELLOW, self.win, 510, 90)
+            self.draw_text(user_input1, self.font3, YELLOW, self.win, 510, 90)
+            self.draw_text(user_input2, self.font3, YELLOW, self.win, 510, 130)
             pygame.display.update()
             mainClock.tick(10)
 
@@ -435,7 +479,7 @@ class Agneepath:
         count = 0
         delay = False
         run = True
-        print("Thread %s starts:" %name, datetime.now())
+        print("%s starts:" %name, datetime.now())
         while run:
             draw(self.win, grid, TOTAL_ROWS, WIDTH)
             if count > 0:
@@ -493,7 +537,7 @@ class Agneepath:
                         if start is end:
                             end.is_start = False
                             end.make_end()
-                            print("Thread %s ends:" %name, datetime.now())
+                            print("%s ends:" %name, datetime.now())
                             run = False
                 if not astar_path and not find_path:
                     raise Exception("Cannot find astar_path.")
